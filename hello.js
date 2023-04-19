c375d7841dad08b4492cc028a8d3228f8707918f
HamzaAli303 <alihamza.m1999@gmail.com>
2023-04-18 21:51:52 -0600

    assesment-2

--- hello.js
+++ hello.js
@@ -1,1 +1,16 @@
 console.log(`You're ready to begin!`)
+
+
+/*        EXTRA CREDIT!
+
+Facebook =  =>Facebook is built in PHP which is compiled with HipHop for PHP used in front-end.
+            =>Erlang is used for Chat, Java and C++. 
+            =>Facebook also uses Linux.
+
+Apple    =  =>Apple mainly uses swift. Swift is a high-level general-purpose, multi-paradigm, that is compalied by Apple themselves. 
+            =>Python, SQL, NoSQL, Java, Scala, C++, C, C#, Object-C. These are other programs that is used in Apple but swift is the most used. 
+            =>Hive, Spark, Kafka, Pyspark, AWS and XCode. These frameworks are needed in Apple to work with different technology. 
+
+Netflix  =  =>Front-end is programmed with javascript. 
+            =>Backend is programmed with Java and Scala. 
+            =>Netflix include Node. js, React, MySQL, PostgreSQL, Amazon RDS, Presto, CloudBees, Pig, Amazon EC2, Amazon S3, Cassandra.
